2bdf14cff1bc766bd8b5350fc3b2ca240b945846
Rachit Khurana <70265590+notnotrachit@users.noreply.github.com>
2024-07-10 21:05:36 +0530

    Update settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5)y-(dzo6u-6rk#yxk2^h0@z(zb!agkor%5_-43od)s!l3432b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["rachit-todo.azurewebsites.net]
 
 
 # Application definition
